7ecb514656899276abca6607676f431d22ea21b0
Lucas  de Oliveira Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2022-01-29 15:08:13 -0300

    Introduction to css

---
+++ frontend/app/routes/jokes.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "remix";
+import styledUrl from '../styles/jokes.css';
+
+export function links() {
+    return [ {
+        rel: "stylesheet",
+        href: styledUrl
+    }]
+}
+
+export default function JokesRoute() {
+  return (
+    <div>
+      <h1>J🤪KES</h1>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
